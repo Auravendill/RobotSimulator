0ef491c76a41c9b3c9469e95571bf4f2d25ad817
Daniel <Daniel-Wolters@gmx.de>
2019-02-19 11:25:49 +0100

    changed to 8 fps fps is easier to change code looks a little bit more beautiful

--- Main.java
+++ Main.java
@@ -1,32 +1,34 @@
-import java.awt.BorderLayout;
+
+//import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+//import java.awt.Graphics2D;
 
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
+//import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.Line2D;
-import java.util.Arrays;
-import java.util.Collections;
+//import java.awt.geom.Line2D;
+//import java.util.Arrays;
+//import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JPanel implements KeyListener {
-	static double Vl =0;
-	static double Vr =0;
-	static double Angle =0;
+	static double Vl = 0;
+	static double Vr = 0;
+	static double Angle = 0;
 	static double Xpos = 100;
-	static double Ypos =200;
-	static double deltaTime = 0.25;
-	static int radius = 15;//15 pixels
+	static double Ypos = 200;
+	static double deltaTime = 0.125;
+	static int radius = 15;// 15 pixels
 	static boolean itterate = true;
 	UseSensors sensor = new UseSensors();
+
 	public static void main(String[] args) {
 
 		JFrame frame = new JFrame("Robot Controller");
@@ -36,7 +38,7 @@ public class Main extends JPanel implements KeyListener {
 
 		frame.setSize(600, 400);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+		frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
 
 		frame.setVisible(true);
 
@@ -47,187 +49,179 @@ public class Main extends JPanel implements KeyListener {
 		CircleIntersections intersect = new CircleIntersections();
 		// containing three elements: position x,y and angle
 
+		// Xpos = 150;
+		// Angle=0;
+		// frame.repaint();
 
-		
-		
-		//Xpos = 150;
-		//Angle=0;
-		//frame.repaint();
-		
-		while(itterate) {
+		while (itterate) {
 			try {
-				Thread.sleep(250);
+				Thread.sleep((int) (1000 * deltaTime));
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//System.out.println(Xpos);
+			// System.out.println(Xpos);
 			motion = new Motion(Xpos, Ypos, Math.toRadians(Angle), Vl, Vr, deltaTime);
 
 			double[] NewPos = motion.motion();
-			//if(collision(NewPos[0], NewPos[1]) == false) {
+			// if(collision(NewPos[0], NewPos[1]) == false) {
 			boolean collision = false;
-			int colidedWall=-1;
-			int colisionCounter =0;
-			
-			for(int i=0; i<walls.length;i++) {
-				List<Point> p = intersect.getCircleLineIntersectionPoint(new Point(walls[i][0], walls[i][1]),new Point(walls[i][2], walls[i][3]), new Point(NewPos[0], NewPos[1]), radius);
-				if(p.size()>0) {
-					
-					
-					
-					if((p.get(0).x >= Math.min(walls[i][0], walls[i][2]) &&p.get(0).x <= Math.max(walls[i][0], walls[i][2])) && (p.get(0).y >= Math.min(walls[i][1], walls[i][3]) &&p.get(0).y <= Math.max(walls[i][1], walls[i][3]))   ) {
+			int colidedWall = -1;
+			int colisionCounter = 0;
+
+			for (int i = 0; i < walls.length; i++) {
+				List<Point> p = intersect.getCircleLineIntersectionPoint(new Point(walls[i][0], walls[i][1]),
+						new Point(walls[i][2], walls[i][3]), new Point(NewPos[0], NewPos[1]), radius);
+				if (p.size() > 0) {
+
+					if ((p.get(0).x >= Math.min(walls[i][0], walls[i][2])
+							&& p.get(0).x <= Math.max(walls[i][0], walls[i][2]))
+							&& (p.get(0).y >= Math.min(walls[i][1], walls[i][3])
+									&& p.get(0).y <= Math.max(walls[i][1], walls[i][3]))) {
 						collision = true;
 						colisionCounter++;
 						colidedWall = i;
 
-						//there is a collision
-						
+						// there is a collision
+
 					}
-					if(p.size()>1 &&(p.get(1).x >= Math.min(walls[i][0], walls[i][2]) &&p.get(1).x <= Math.max(walls[i][0], walls[i][2])) && (p.get(1).y >= Math.min(walls[i][1], walls[i][3]) &&p.get(1).y <= Math.max(walls[i][1], walls[i][3]))   ) {
+					if (p.size() > 1
+							&& (p.get(1).x >= Math.min(walls[i][0], walls[i][2])
+									&& p.get(1).x <= Math.max(walls[i][0], walls[i][2]))
+							&& (p.get(1).y >= Math.min(walls[i][1], walls[i][3])
+									&& p.get(1).y <= Math.max(walls[i][1], walls[i][3]))) {
 						collision = true;
 						colisionCounter++;
 						colidedWall = i;
 
-						//there is a collision
-						
+						// there is a collision
+
 					}
-						
+
 				}
 			}
-			if(collision == false) {
-			Xpos = NewPos[0];
-			
-			
-			Ypos = NewPos[1];
-			
-			Angle = Math.toDegrees(NewPos[2]);
-			
-			}
-			else {
-				
-				if(colisionCounter== 2) {
-					if(walls[colidedWall][0]== walls[colidedWall][2] && colidedWall != -1) {//vertical wall
-						
+			if (collision == false) {
+				Xpos = NewPos[0];
+
+				Ypos = NewPos[1];
+
+				Angle = Math.toDegrees(NewPos[2]);
+
+			} else {
+
+				if (colisionCounter == 2) {
+					if (walls[colidedWall][0] == walls[colidedWall][2] && colidedWall != -1) {// vertical wall
+
 						Ypos = NewPos[1];
-						
+
 						Angle = Math.toDegrees(NewPos[2]);
-					}
-					else if (walls[colidedWall][1]== walls[colidedWall][3] && colidedWall != -1) {
+					} else if (walls[colidedWall][1] == walls[colidedWall][3] && colidedWall != -1) {
 						Xpos = NewPos[0];
-						
+
 						Angle = Math.toDegrees(NewPos[2]);
 					}
 				}
 			}
-			
-			
-			if(Angle>=360) {
-				Angle =Angle - 360;
+
+			if (Angle >= 360) {
+				Angle = Angle - 360;
 			}
-			if(Angle<=-360) {
-				Angle =Angle + 360;
+			if (Angle <= -360) {
+				Angle = Angle + 360;
 			}
-			//System.out.println("(" + Xpos + ", " + Ypos + ")");
+			// System.out.println("(" + Xpos + ", " + Ypos + ")");
 			frame.repaint();
-			//}
-			//else {
-			//do collision things
-			//draw(Xpos, Ypos, Angle);
-			//}
+			// }
+			// else {
+			// do collision things
+			// draw(Xpos, Ypos, Angle);
+			// }
 		}
 
 	}
-    
 
-
-	public void paint(Graphics g){
+	public void paint(Graphics g) {
 
 		Walls w = new Walls();
 		int[][] walls = w.getWalls();
-		for(int z=0;z< walls.length;z++) {
+		for (int z = 0; z < walls.length; z++) {
 			g.drawLine(walls[z][0], walls[z][1], walls[z][2], walls[z][3]);
 		}
 
-		g.drawOval((int)Xpos-radius,(int)Ypos-radius,radius*2,radius*2);
+		g.drawOval((int) Xpos - radius, (int) Ypos - radius, radius * 2, radius * 2);
 
 		g.setColor(Color.RED);
 
-		g.drawLine((int)Xpos, (int)Ypos,(int)( Xpos+radius* Math.sin(Math.toRadians(Angle+90))),(int)( Ypos+radius* Math.cos(Math.toRadians(Angle+90))));
+		g.drawLine((int) Xpos, (int) Ypos, (int) (Xpos + radius * Math.sin(Math.toRadians(Angle + 90))),
+				(int) (Ypos + radius * Math.cos(Math.toRadians(Angle + 90))));
 		g.setColor(Color.BLACK);
 		double[] distances = new double[12];
 		distances = sensor.GetDistances(Xpos, Ypos, Angle, radius);
-		for(int i=0; i<12;i++) {
-			double tempAngle = Angle +90+i*30;
-			if(tempAngle> 360) {
-				tempAngle = tempAngle-360;
+		for (int i = 0; i < 12; i++) {
+			double tempAngle = Angle + 90 + i * 30;
+			if (tempAngle > 360) {
+				tempAngle = tempAngle - 360;
 			}
-			double xText =0;
-			if(tempAngle-90 >90 &&tempAngle-90 <270) {
-				xText = (Xpos+(radius+20)* Math.sin(Math.toRadians(tempAngle)));
+			double xText = 0;
+			if (tempAngle - 90 > 90 && tempAngle - 90 < 270) {
+				xText = (Xpos + (radius + 20) * Math.sin(Math.toRadians(tempAngle)));
+			} else {
+				xText = (Xpos + (radius + 10) * Math.sin(Math.toRadians(tempAngle)));
 			}
-			else {
-				xText = (Xpos+(radius+10)* Math.sin(Math.toRadians(tempAngle)));
-			}
-			double yText = (Ypos+(radius+15)* Math.cos(Math.toRadians(tempAngle)));
-		g.drawString(Integer.toString((int) distances[i]),(int) xText ,(int) yText);
+			double yText = (Ypos + (radius + 15) * Math.cos(Math.toRadians(tempAngle)));
+			g.drawString(Integer.toString((int) distances[i]), (int) xText, (int) yText);
 		}
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int id = e.getID();
 		String keyString;
 		if (id == KeyEvent.KEY_PRESSED) {
 			char c = e.getKeyChar();
-			if("w".contentEquals(Character.toString(c))) {
+			if ("w".contentEquals(Character.toString(c))) {
 				Vl = Vl + 1;
-			}
-			else if("s".contentEquals(Character.toString(c))) {
+			} else if ("s".contentEquals(Character.toString(c))) {
 				Vl = Vl - 1;
-			}
-			else if("o".contentEquals(Character.toString(c))) {
+			} else if ("o".contentEquals(Character.toString(c))) {
 				Vr = Vr + 1;
-			}
-			else if("l".contentEquals(Character.toString(c))) {
+			} else if ("l".contentEquals(Character.toString(c))) {
 				Vr = Vr - 1;
-			}
-			else if("x".contentEquals(Character.toString(c))) {
+			} else if ("x".contentEquals(Character.toString(c))) {
 				Vr = 0;
 				Vl = 0;
-			}
-			else if("t".contentEquals(Character.toString(c))) {
+			} else if ("t".contentEquals(Character.toString(c))) {
 				Vr = Vr + 1;
 				Vl = Vl + 1;
-			}
-			else if("g".contentEquals(Character.toString(c))) {
+			} else if ("g".contentEquals(Character.toString(c))) {
 				Vr = Vr - 1;
 				Vl = Vl - 1;
 			}
-			if(Vr >5) {
+			if (Vr > 5) {
 				Vr = 5;
 			}
-			if(Vl >5) {
+			if (Vl > 5) {
 				Vl = 5;
 			}
-			if(Vr <-5) {
+			if (Vr < -5) {
 				Vr = -5;
 			}
-			if(Vl <-5) {
+			if (Vl < -5) {
 				Vl = -5;
 			}
-			System.out.println("Vl = "+Vl);
-			System.out.println("Vr = "+Vr);
-
-
+			System.out.println("Vl = " + Vl);
+			System.out.println("Vr = " + Vr);
 
 		}
 
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
