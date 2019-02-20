a038bb4c41793774dcde710f4f7945ae4cd3f96e
Auravendill <Daniel-Wolters@gmx.de>
2019-02-20 12:31:43 +0100

    added constructor array can be used as 2D with function added mutation (for generating a child)

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -1,13 +1,49 @@
+import java.util.concurrent.ThreadLocalRandom;
 
 public class NeuralNetwork {
 	private double[] weights;
 
-	public NeuralNetwork(double inputNodes, double hiddenNodes, double outputNodes, boolean start) {
+	int inputNodes, hiddenNodes, outputNodes;
 
+	private static final double[] startValues = { -1.0, 1.0 };
+
+	public NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes, boolean start) {
+		weights = new double[inputNodes + hiddenNodes + outputNodes];
+		this.inputNodes = inputNodes;
+		this.hiddenNodes = hiddenNodes;
+		this.outputNodes = outputNodes;
+
+		if (start) {
+			for (int i = 0; i < weights.length; ++i) {
+				double value = ThreadLocalRandom.current().nextDouble(startValues[0], startValues[1]);
+				weights[i] = value;
+			}
+		}
+	}
+
+	private double getNode(int layer, int number) {
+		switch (layer) {
+		case 0:// inputLayer
+			if (number < inputNodes) {
+				return weights[number];
+			} else {
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		case 1:// hiddenLayer
+			if (number < inputNodes + hiddenNodes) {
+				return weights[inputNodes + number];
+			} else {
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		case 2:// outputLayer
+			return weights[inputNodes + hiddenNodes + number];
+		default:
+			throw new ArrayIndexOutOfBoundsException();
+		}
 	}
 
 	public double[] getOutput(double[] inputs) {
-
+		return null;// placeholder
 	}
 
 	public double[] getWeights() {
@@ -19,11 +55,19 @@ public class NeuralNetwork {
 	}
 
 	public NeuralNetwork getChild(NeuralNetwork parent) {
+		NeuralNetwork child = new NeuralNetwork(inputNodes, hiddenNodes, outputNodes, false);
 
+		return child;
 	}
 
-	private void mutation() {
+	private void mutate(double mutationChance, double radiation) {
+		for (int i = 0; i < weights.length; ++i) {
+			double rnd = Math.random();
+			if (rnd < mutationChance) {
+				double geneChange = ThreadLocalRandom.current().nextDouble(-1 * radiation, radiation);
+				weights[i] += geneChange;
+			}
+		}
 
 	}
-
 }
