81c14ffad99ba1f5350d1b9d4a534c34d204c3b3
Daniel <Daniel-Wolters@gmx.de>
2019-02-20 11:27:59 +0100

    added NeuralNetwork (only structure)

---
+++ NeuralNetwork.java
@@ -0,0 +1,29 @@
+
+public class NeuralNetwork {
+	private double[] weights;
+
+	public NeuralNetwork(double inputNodes, double hiddenNodes, double outputNodes, boolean start) {
+
+	}
+
+	public double[] getOutput(double[] inputs) {
+
+	}
+
+	public double[] getWeights() {
+		return weights;
+	}
+
+	public void setWeights(double[] weights) {
+		this.weights = weights;
+	}
+
+	public NeuralNetwork getChild(NeuralNetwork parent) {
+
+	}
+
+	private void mutation() {
+
+	}
+
+}
